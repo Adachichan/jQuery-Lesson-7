bafaf96df04bd988bd2c75b2409864434d5d2748
Adachichan <129758854+Adachichan@users.noreply.github.com>
2024-01-02 13:57:10 +0000

    [add] #2 Chapter6_class_part jsファイルへのコメント追加

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// 
-
+// class属性の切り替え → addClass( )とremoveClass( )
+// addClass( )：対象のHTML要素にclass属性を追加する。
+// removeClass( )：要素に設定されているclass属性を解除する。
 
 $(function(){
   $('.box1').mouseover(function(){
